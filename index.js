eca009833cb20e6dbecbb0fcf00b78dff0c9b829
Bonniehuo <17717375242@163.com>
2021-08-08 23:36:57 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+//console.log(100)
+// 变量
+let title = "Alert: "  //  string
+let title1 = "Error "
+let title2 = "Success"
+//console.log(myName)
+let alert1 = title + title1
+console.log(alert1);
+let alert2 = title + title2
+console.log(alert2);
+let score = 100  // number
+console.log(score)
+
+let isFalse = true
+console.log(isFalse)
+
+// null & undefined
+let bag = undefined
+console.log(bag)
+
+let bag2 = null
+console.log(bag2)
+
+// let var const
+// let >> var
+let a1 = "aaa"
+console.log(a1);
+// update content of a1
+a1 = "bbb"
+console.log(a1);
+
+const b1 = "aaa" 
+console.log(b1);
+// update content of a1
+// b1 = "bbb"
+// console.log(b1);
+
+// var vs let
+// var is function scoped and let is block scoped.
+console.log(x);
+var x = 5
+console.log(x);
+
+// Uncaught ReferenceError: Cannot access 'y' before initialization
+console.log(y);
+let y = 10
+console.log(y);
+// 
+// var 声明全局变量，变量在声明前也可以使用，只是是 undefined 类型，
+// let 声明块区域的局部变量，只有在声明后才起作用，不然会报错
+
+
+
